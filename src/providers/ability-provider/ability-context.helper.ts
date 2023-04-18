9e0aadf5fb907f416fd21a2e9bd8629b18c13f45
Jamal Soueidan <jamal@soueidan.com>
2023-04-18 10:13:56 +0200

    add ability for admin page

---
+++ src/providers/ability-provider/ability-context.helper.ts
@@ -0,0 +1,47 @@
+import { AbilityBuilder, createMongoAbility } from "@casl/ability";
+import { AbilityContextType, AbilityUser } from "./ability-context";
+
+export const defineAbilityFor = (user: AbilityUser): AbilityContextType => {
+  const { can, build } = new AbilityBuilder<AbilityContextType>(
+    createMongoAbility
+  );
+
+  if (user.isOwner) {
+    can("manage", "product");
+    can("manage", "staff");
+    can("manage", "collection");
+  }
+
+  if (user.isAdmin) {
+    can("manage", "staff");
+    can("update", "product");
+  }
+
+  if (user.isUser) {
+    can("update", "staff", { _id: user.userId });
+  }
+
+  return build();
+};
+
+export const getAbilityFromToken = () => {
+  const parse = parseJwt(localStorage.getItem("token") || "");
+  return defineAbilityFor(parse);
+};
+
+function parseJwt(token: string) {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split("")
+      // eslint-disable-next-line func-names
+      .map(function (c) {
+        return `%${`00${c.charCodeAt(0).toString(16)}`.slice(-2)}`;
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+}
