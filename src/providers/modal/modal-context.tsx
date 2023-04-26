e814e7e8d946447163b18759be4794b8ee7249d6
Jamal Soueidan <jamal@soueidan.com>
2023-04-26 21:58:33 +0200

    chore: remove staticwebapp.config.json file refactor(modal-context.hook.ts, modal-context.tsx): rename ModalContextType to ModalContextProps to better reflect the type of the context object and its properties.

--- src/providers/modal/modal-context.tsx
+++ src/providers/modal/modal-context.tsx
@@ -4,8 +4,8 @@ import { createContext } from "react";
 export type ModalProps = MP & {
   content: string;
 };
-export type ModalContextAction = ModalProps & {
+export type ModalContextProps = ModalProps & {
   update: (modal: Partial<ModalProps>) => void;
 };
 
-export const ModalContext = createContext<ModalContextAction>({} as never);
+export const ModalContext = createContext<ModalContextProps>({} as never);
